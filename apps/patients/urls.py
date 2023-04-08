fb2789c289320f34dee7a7b8c562f395f25d0574
adcc662 <david@DESKTOP-JH1BAAU>
2023-04-08 11:31:03 -0600

    Checking the list of patients

--- apps/patients/urls.py
+++ apps/patients/urls.py
@@ -5,12 +5,10 @@ from .views import (
     PatientAlergyListCreate,
     PatientAlergyRetrieveUpdateDestroy,
 )
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
+
 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('patients/', PatientList.as_view(), name='patient_list_create'),
     path('patients/<int:pk>/', PatientRetrieveUpdateDestroy.as_view(), name='patient_retrieve_update_destroy'),
     path('patients-alergy/', PatientAlergyListCreate.as_view(), name='patient_alergy_list_create'),
--- patient/urls.py
+++ patient/urls.py
@@ -21,5 +21,7 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.patients.urls')),
+     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
